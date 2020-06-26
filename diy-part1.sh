262560e85c1930dc191697bf1dff5c46dfd116ca
hvvy <56221924+hvvy@users.noreply.github.com>
2020-06-26 23:53:41 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,3 +13,11 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+git clone https://github.com/Leo-Jo/luci-app-koolproxyR.git package/luci-app-koolproxyR
+git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
+git clone https://github.com/vernesong/OpenClash package/luci-app-openclash
+git clone https://github.com/fw876/helloworld.git package/helloworld
+./scripts/feeds update -a
+./scripts/feeds install -a
